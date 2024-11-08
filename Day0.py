0c9b9c2ba63a20d8f279f5b8e5696dcb2bfba258
aryan <aryan0312g@outlook.com>
2024-11-08 21:03:49 +0530

    added comments in Day0

--- Day0.py
+++ Day0.py
@@ -1,12 +1,12 @@
 import requests
 
-num1 = input();
-num2 = input();
-operator = input();
-data = {"expr":num1+operator+num2}
-response = requests.post('https://api.mathjs.org/v4/',json=data)
+num1 = input();  # taking the first number and second number argument as input
+num2 = input(); 
+operator = input(); # the operation to perform
+data = {"expr":num1+operator+num2}  # creating the type of data as accepted by the api
+response = requests.post('https://api.mathjs.org/v4/',json=data)   # creating the request
 
-if(response.status_code == 200):
+if(response.status_code == 200): # means the fetching was succesfull
     print(response.json())
 else:
     print(response.status_code)
